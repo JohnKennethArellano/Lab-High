11f994d357ecb9bd6d4f7ebc2f274ec878b8af27
Nicole Santos <nicole.santos.salamat@bulsu.edu.ph>
2024-03-23 17:25:53 +0800

    dashboard charts fixed

--- src/main.js
+++ src/main.js
@@ -6,10 +6,10 @@ import './axios/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import { Icon } from '@iconify/vue'
+import { Icon } from '@iconify/vue'
 const app = createApp(App)
 
 app.use(store)
 app.use(router)
-// app.component('Icon', Icon)
+app.component('Icon', Icon)
 app.mount('#app')
